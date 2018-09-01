799bc076efb1d51d9264f86727f6bbc8ba9af5e4
Yunseop Kim <undefined@mealant.com>
2018-09-01 14:35:12 +0900

    implements startServer

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,11 +1,16 @@
 import { request } from "graphql-request";
-import { host } from "./constants";
 import { User } from "../entity/User";
-import { createTypeormConn } from "../utils/createTypeOrmConnections";
+import { startServer } from "../startServer";
+
+let getHost = () => "";
 
 beforeAll(async () => {
-    await createTypeormConn();
+  // await createTypeormConn();
+  const app = await startServer();
+  const { port }: any = app.address();
+  getHost = () => `http://127.0.0.1:${port}`;
 });
+
 const email = "myname@name.com";
 const password = "123456";
 const mutation = `
@@ -13,7 +18,7 @@ mutation {
     register(email: "${email}", password:"${password}")
 }`;
 test("Register user", async () => {
-  const response = await request(host, mutation);
+  const response = await request(getHost(), mutation);
   expect(response).toEqual({ register: true });
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
