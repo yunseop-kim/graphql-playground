4a0902e22dec5e1ad12ed860c8b76c136688ae46
Yunseop Kim <undefined@mealant.com>
2018-09-01 13:33:02 +0900

    add register user test code.

--- src/tests/register.test.ts
+++ src/tests/register.test.ts
@@ -1,7 +1,22 @@
-function sum(a: number, b: number) {
-  return a + b;
-}
+import { request } from "graphql-request";
+import { host } from "./constants";
+import { createConnection } from "typeorm";
+import { User } from "../entity/User";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+const email = "myname@name.com";
+const password = "123456";
+const mutation = `
+mutation {
+    register(email: "${email}", password:"${password}")
+}`;
+test("Register user", async () => {
+  const response = await request(host, mutation);
+  expect(response).toEqual({ register: true });
+
+  await createConnection();
+  const users = await User.find({ where: { email } });
+  expect(users).toHaveLength(1);
+  const user = users[0];
+  expect(user.email).toEqual(email);
+  expect(user.password).toEqual(password);
 });
