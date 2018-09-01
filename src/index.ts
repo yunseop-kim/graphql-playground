c10fd3c983fc8f492201cbcde5c0d4706c0f0a44
Yunseop Kim <undefined@mealant.com>
2018-09-01 11:40:39 +0900

    seperate resolver, schema.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,8 @@
 import "reflect-metadata";
+import { importSchema } from "graphql-import";
 import { GraphQLServer } from "graphql-yoga";
-
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`;
-
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || "World"}`
-  }
-};
+import { resolvers } from "./resolvers";
+const typeDefs = importSchema("src/schema.graphql"); // graphql-import's bug, have to use absolute path.
 
 const server = new GraphQLServer({ typeDefs, resolvers });
 server.start(() => console.log("Server is running on localhost:4000"));
