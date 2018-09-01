e6c4c776e7cd1dc859a0d4a5052842b5b9c27ea0
Yunseop Kim <undefined@mealant.com>
2018-09-01 15:27:39 +0900

    add query.

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -1,14 +1,17 @@
-import * as bcrypt from 'bcryptjs'
-import { ResolverMap } from '../../types/graphql-utils';
-import { User } from '../../entity/User';
+import * as bcrypt from "bcryptjs";
+import { ResolverMap } from "../../types/graphql-utils";
+import { User } from "../../entity/User";
 
 export const resolvers: ResolverMap = {
+  Query: {
+    bye: () => "bye"
+  },
   Mutation: {
     register: async (_, { email, password }) => {
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = User.create({
-         email,
-         password: hashedPassword
+        email,
+        password: hashedPassword
       });
 
       await user.save();
