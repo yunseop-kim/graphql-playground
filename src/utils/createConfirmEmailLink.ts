12e897e2f0ca8634a746a3f541669056eaf75855
Yunseop Kim <undefined@mealant.com>
2018-09-04 22:25:35 +0900

    add util

---
+++ src/utils/createConfirmEmailLink.ts
@@ -0,0 +1,16 @@
+import { v4 } from "uuid";
+import { Redis } from "ioredis";
+import { ObjectID } from "typeorm";
+// http://localhost:4000
+// http://my-site.com
+// http://my-site.com/confirm/<id>
+
+export const createConfirmEmailLink = async (
+  url: string,
+  userId: ObjectID,
+  redis: Redis
+) => {
+  const id = v4();
+  redis.set(id, userId, "ex", 60 * 60 * 24);
+  return `${url}/confirm/${id}`
+};
