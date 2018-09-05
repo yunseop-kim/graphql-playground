b4d8d4612744710cc69cbd2d7e9fc64f76c886d2
Yunseop Kim <undefined@mealant.com>
2018-09-05 23:56:31 +0900

    remove async

--- src/utils/createConfirmEmailLink.ts
+++ src/utils/createConfirmEmailLink.ts
@@ -5,12 +5,12 @@ import { ObjectID } from "typeorm";
 // http://my-site.com
 // http://my-site.com/confirm/<id>
 
-export const createConfirmEmailLink = async (
+export const createConfirmEmailLink = (
   url: string,
   userId: ObjectID,
   redis: Redis
 ) => {
   const id = v4();
   redis.set(id, userId, "ex", 60 * 60 * 24);
-  return `${url}/confirm/${id}`
+  return `${url}/confirm/${id}`;
 };
