9d0ad074d7c347b9bd4b6c4d3625014271b40a4f
Yunseop Kim <undefined@mealant.com>
2018-09-06 20:08:16 +0900

    indent.

--- src/modules/register/register.test.ts
+++ src/modules/register/register.test.ts
@@ -24,14 +24,20 @@ mutation {
 }`;
 describe("Register user", async () => {
   it("check for the duplicate emails", async () => {
-    const response = await request(process.env.TEST_HOST as string, mutation(email, password));
+    const response = await request(
+      process.env.TEST_HOST as string,
+      mutation(email, password)
+    );
     expect(response).toEqual({ register: null });
     const users = await User.find({ where: { email } });
     expect(users).toHaveLength(1);
     const user = users[0];
     expect(user.email).toEqual(email);
     expect(user.password).not.toEqual(password);
-    const response2: any = await request(process.env.TEST_HOST as string, mutation(email, password));
+    const response2: any = await request(
+      process.env.TEST_HOST as string,
+      mutation(email, password)
+    );
     expect(response2.register).toHaveLength(1);
     expect(response2.register[0]).toEqual({
       path: "email",
@@ -40,7 +46,10 @@ describe("Register user", async () => {
   });
 
   it("catch bad email", async () => {
-    const response3: any = await request(process.env.TEST_HOST as string, mutation("b", password));
+    const response3: any = await request(
+      process.env.TEST_HOST as string,
+      mutation("b", password)
+    );
     expect(response3).toEqual({
       register: [
         {
@@ -56,7 +65,10 @@ describe("Register user", async () => {
   });
 
   it("catch bad password", async () => {
-    const response4: any = await request(process.env.TEST_HOST as string, mutation(email, "ad"));
+    const response4: any = await request(
+      process.env.TEST_HOST as string,
+      mutation(email, "ad")
+    );
     expect(response4).toEqual({
       register: [
         {
@@ -68,7 +80,10 @@ describe("Register user", async () => {
   });
 
   it("catch bad password and bad eamil", async () => {
-    const response5: any = await request(process.env.TEST_HOST as string, mutation("bs", "ad"));
+    const response5: any = await request(
+      process.env.TEST_HOST as string,
+      mutation("bs", "ad")
+    );
     expect(response5).toEqual({
       register: [
         {
