ab434ae7fa3db599e2b077674233525256045011
Yunseop Kim <undefined@mealant.com>
2018-09-06 20:07:32 +0900

    change types

--- src/testSetup/setup.ts
+++ src/testSetup/setup.ts
@@ -1,8 +1,9 @@
 import { startServer } from "../startServer";
+import { AddressInfo } from "net";
 
 export const setup = async () => {
   // await createTypeormConn();
   const app = await startServer();
-  const { port }: any = app.address();
+  const { port }: AddressInfo = app.address() as AddressInfo;
   process.env.TEST_HOST = `http://127.0.0.1:${port}`;
 };
