22a0f398f6259fa7b26f004053f99bf1b0919b84
Yunseop Kim <undefined@mealant.com>
2018-09-06 21:02:17 +0900

    seperate genSchema code.

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,26 +1,12 @@
-import "reflect-metadata";
-import { importSchema } from "graphql-import";
-import { GraphQLServer } from "graphql-yoga";
-import * as path from "path";
-import * as fs from "fs";
+import "reflect-metadata"
+import { GraphQLServer } from "graphql-yoga"
 import { createTypeormConn } from "./utils/createTypeOrmConnections";
-import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
-// tslint:disable-next-line:no-implicit-dependencies
-import { GraphQLSchema } from "graphql";
 import { redis } from "./redis";
 import { confirmEmail } from "./routes/confirmEmail";
+import { genSchema } from "./utils/genSchema";
 export const startServer = async () => {
-  const schemas: GraphQLSchema[] = [];
-  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
-  folders.forEach(folder => {
-    const { resolvers } = require(`./modules/${folder}/resolvers`);
-    const typeDefs = importSchema(
-      path.join(__dirname, `./modules/${folder}/schema.graphql`)
-    );
-    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
-  });
   const props: any = {
-    schema: mergeSchemas({ schemas }),
+    schema: genSchema,
     context: ({ request }: any) => ({
       redis,
       url: `${request.protocol}://${request.get("host")}`
